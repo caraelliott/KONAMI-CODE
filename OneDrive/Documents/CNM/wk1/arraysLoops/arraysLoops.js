79e9cc1d956964351870c59506d559f4335a37d2
Cara Elliott <caraelliottwgsg@gmail.com>
2021-08-27 11:42:04 +0100

    your message

---
+++ OneDrive/Documents/CNM/wk1/arraysLoops/arraysLoops.js
@@ -0,0 +1,62 @@
+let coffeeOrder = [
+    "Alex - Cortado",
+    "Ben - Cortado",
+    "Charlie - Whatever's new"
+];
+
+console.log(coffeeOrder);
+
+console.log(coffeeOrder[2]);
+
+let favGames = [
+    "Link's Awakening",
+    "Streets of Rage 1,2,4",
+    "Altered Beast",
+    "Panzer Paladin",
+    "EDF",
+    "Shakedown Hawaii",
+    "Ocarina of Time",
+    "Final Fight",
+    "Simpsons Arcade Game",
+    "X-Men Arcade Game",
+    "DnD Chronicles of Mystara",
+    "Captain Commando",
+    "Metal Slug 3",
+    "Knights of the Round"
+]
+
+console.log(favGames);
+
+let toBuy = [
+    "Cyberpunk 2077 XBONE",
+    "Aliens: Fireteam Elite",
+    "River City Girls 2",
+    "Klonoa 1,2",
+    "Legend of Zelda: Oracle of Seasons/Ages",
+    "Daemon x Machina",
+    "Witcher 2",
+    "TMNT: Shredder's Revenge",
+]
+
+console.log(toBuy);
+
+let favSongs = [
+    "Radiohead - Idioteque",
+    "Failure - Another Space Song",
+    "Slowdive - Souvlaki Space Station",
+
+]
+console.log(favSongs);
+
+favSongs.push("Ride - Drive Blind");
+favSongs.push("Deerhunter - Agoraphobia");
+
+console.log(favSongs);
+
+favSongs.push("Sonic Youth - Tunic (Song for Karen)");
+favSongs.push("Misfits - Bullet");
+
+console.log(favSongs);
+
+favSongs.splice(6,1);
+console.log(favSongs);
